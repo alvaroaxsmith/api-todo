1331ee9705b5347b4f48f76250154f6e00bd5d01
alvaroun <alvaromachadoferreira@hotmail.com>
2022-08-09 20:37:33 -0300

    feat: add crud

--- src/app/todo/todo.controller.ts
+++ src/app/todo/todo.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { TodoService } from './todo.service';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseUUIDPipe, Post, Put } from '@nestjs/common';
 
 @Controller('api/v1/todo')
-export class TodoController {}
+export class TodoController {
+    constructor(private readonly todoService: TodoService) {}
+
+    @Get()
+    async index() {
+        return await this.todoService.findAll();
+    }
+
+    @Post()
+    async create(@Body() body) {
+        return await this.todoService.create(body);
+    }
+
+    @Get(':id')
+    async show(@Param('id', new ParseUUIDPipe()) id: string) {
+        return await this.todoService.findOneOrFail(id);
+    }
+
+    @Put(':id')
+    async update(@Param('id', new ParseUUIDPipe()) id: string, @Body() body) {
+        return await this.todoService.update(id, body);
+    }
+
+    @Delete(':id')
+    @HttpCode(HttpStatus.NO_CONTENT)
+    async destroy(@Param('id', new ParseUUIDPipe()) id: string) {
+        await this.todoService.deleteById(id);
+    }
+}
